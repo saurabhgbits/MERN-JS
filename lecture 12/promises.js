9152549fcbf09a9533ee8afe30b6bd070d2a0056
Saurabh Gupta <saurabhg.bits@gmail.com>
2023-06-26 12:42:05 +0530

    nothing to worry

--- lecture 12/promises.js
+++ lecture 12/promises.js
@@ -2,13 +2,14 @@ const egg = new Promise(resolve,reject) => {
     console.log('executor');
     resolve('successfully hatch');
     reject('fail');
-}
+     }
 
 
 egg 
- .then(value =>{
-    console.log()
+      .then(value) =>{
+      console.log()
 
- }
+     }
+ 
 
- )
+ 
